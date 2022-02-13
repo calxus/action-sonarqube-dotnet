e37c0b18042e4dd79adb53c1d3a3659a94c405e4
Gordon Adam-Santiago <calxus@protonmail.ch>
2022-02-13 15:38:52 +0000

    Adding action

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# action-sonarqube-dotnet
+# SonarScanner for .NET
+
+SonarScanner for .NET for use in Github Actions.
+
+## Use with self-hosted SonarQube
+
+``` yaml
+    - name: SonarScanner for .NET 6
+      uses: recipopdev/action-sonarqube-dotnet@v1.0.0
+      with:
+        sonarProjectKey: your_projectkey
+        sonarProjectName:  your_projectname
+        sonarHostname:  your_selfhosted_sonarqube_hostname
+  env:
+    SONAR_TOKEN: ${{ secrets.SONAR_TOKEN }}
+    GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
+
+# Description of all inputs
+
+``` yaml
+inputs:
+  sonarProjectKey:
+    description: "The key of the SonarQube project"
+    required: true
+  sonarProjectName:
+    description: "The name of the SonarQube project"
+    required: true
+  sonarHostname:
+    description: "The SonarQube server URL. For SonarCloud, skip this setting."
+    default: "https://sonarcloud.io"
+    required: false
+```
---
+++ entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -o pipefail
+set -eu
+
+if [ -z "$INPUT_SONARPROJECTKEY" ]; then
+    echo "Input parameter sonarProjectKey is required"
+    exit 1
+fi
+if [ -z "$INPUT_SONARPROJECTNAME" ]; then
+    echo "Input parameter sonarProjectName is required"
+    exit 1
+fi
+if [ -z "$SONAR_TOKEN" ]; then
+    echo "Environment parameter SONAR_TOKEN is required"
+    exit 1
+fi
+
+echo "INPUT_SONARPROJECTKEY: $INPUT_SONARPROJECTKEY"
+echo "INPUT_SONARPROJECTNAME: $INPUT_SONARPROJECTNAME"
+echo "INPUT_SONARHOSTNAME: $INPUT_SONARHOSTNAME"
+
+sonar_begin_cmd="/dotnet-sonarscanner begin /k:\"${INPUT_SONARPROJECTKEY}\" /n:\"${INPUT_SONARPROJECTNAME}\" /d:sonar.login=\"${SONAR_TOKEN}\" /d:sonar.host.url=\"${INPUT_SONARHOSTNAME}\""
+
+sonar_end_cmd="/dotnet-sonarscanner end /d:sonar.login=\"${SONAR_TOKEN}\""
+
+dotnet_build_cmd="dotnet build"
+
+echo "sonar_begin_cmd: $sonar_begin_cmd"
+sh -c "$sonar_begin_cmd"
+
+echo "dotnet_build_cmd: $dotnet_build_cmd"
+sh -c "${dotnet_build_cmd}"
+
+echo "sonar_end_cmd: $sonar_end_cmd"
+sh -c "$sonar_end_cmd"
